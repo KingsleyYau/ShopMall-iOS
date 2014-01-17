9059274f2561cb3c30d4c9a32e41e2b752e4600e
KingsleyYau <Kingsleyyau@gmail.com>
2014-01-17 21:29:19 +0800

    Revert "add ShopMall source code"
    This reverts commit 876e8a7c8944902bede1e01e0dcc419999f21bd7.

--- ShopMall/Source/Main/ModuleList/EBabyModuleList.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import "EBabyModule.h"
-@interface EBabyModuleList : NSObject {
-    
-}
-@property (nonatomic, readonly) NSArray *modules;
-- (EBabyModule *)moduleForTabBarItem:(UITabBarItem *)aItem;
-@end
